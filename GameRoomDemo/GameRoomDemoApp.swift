0a5ec5001700d28d5bcf90345b032c851d588d6b
cwalter50 <walt50@gmail.com>
2023-03-05 09:43:57 -0500

    Initial Commit

---
+++ GameRoomDemo/GameRoomDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameRoomDemoApp.swift
+//  GameRoomDemo
+//
+//  Created by Christopher Walter on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GameRoomDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
